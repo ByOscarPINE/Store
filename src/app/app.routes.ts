966d4eca726c0dce0547481bcd9354ad43cd31bb
SrGines35 <ginesgm35@gmail.com>
2026-05-27 09:46:25 -0600

    (Fix)Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { NewProduct } from '../components/NewProduct/NewProduct';
+import { Products } from './pages/products/products';
 
 export const routes: Routes = [
     {
@@ -14,5 +15,9 @@ export const routes: Routes = [
     {
         path: "NewProduct",
         component: NewProduct
+    },
+    {
+        path: "Products",
+        component: Products
     }
 ];
